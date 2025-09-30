9738b52a9e33fd0b9583fe10637a6c9f71eb857c
Matias <mativmutte@gmail.com>
2025-09-30 08:37:15 -0300

    config: enable production mode in environments.ts

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environments = {
 
-    production: false,
+    production: true,
     supabaseUrl: 'https://roiozujnbprxcdghcimx.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvaW96dWpuYnByeGNkZ2hjaW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5OTE1NDksImV4cCI6MjA3MzU2NzU0OX0.unKBbSeTn9sXEhdnfGpT4vbHRcaJCvdz1PN_Z8LNUQY'
 
